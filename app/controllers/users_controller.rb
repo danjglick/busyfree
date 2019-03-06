bbe3bfb0256a661fa19cbf63b3dbfd9b9a4cb2ec
Dan Glick <danjglick@Dans-MacBook-Pro.local>
2019-03-06 13:30:04 -0500

    fixed currently_connected conditionals

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,34 +14,34 @@ class UsersController < ApplicationController
     end
   end
 
-  def self.signin(params)
-    is_successful = false
-    if params[:commit] == "Sign in"
-      for i in User.all
-        if (i.name == params[:user][:name] || i.phone == params[:user][:phone]) && i.password == params[:user][:password]
-          is_successful = true
-          @@user = i
-        end
-      end
-    end
-    return is_successful
-  end
-
-  def self.signup(params)
-    is_successful = false
-    if params[:commit] == "Sign up"
-      form_inputs = {name: params[:user][:name], phone: params[:user][:phone], password: params[:user][:password]}
-      @@user = User.new(form_inputs)
-      if @@user.save
-        is_successful = true
-      end
-    end
-    return is_successful
-  end
-
   def show
   end
 
   def edit
+  end
+
+  def self.signin(params)
+    bool = false
+    if params[:commit] == "Sign in"
+      for i in User.all
+        if (i.name == params[:user][:name] || i.phone == params[:user][:phone]) && i.password == params[:user][:password]
+          bool = true
+          @@user = i
+        end
+      end
+    end
+    return bool
+  end
+
+  def self.signup(params)
+    bool = false
+    if params[:commit] == "Sign up"
+      form_inputs = {name: params[:user][:name], phone: params[:user][:phone], password: params[:user][:password]}
+      @@user = User.new(form_inputs)
+      if @@user.save
+        bool = true
+      end
+    end
+    return bool
   end
 end
--- app/models/User.rb
+++ app/models/User.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
     connected = false
     for i in User.all
       should_connect = (self.busy_or_free == "free" && i.busy_or_free == "free" && self.friends.include?(i.name) && i.friends.include?(self.name))
-      currently_connected = (i.connected_to == self.name)
+      currently_connected = (self.connected_to == i.name || i.connected_to == self.name)
       recently_connected = (i.name == self.just_connected || i.just_connected == self.name)
       if (should_connect || currently_connected) && !recently_connected
         connected = true
