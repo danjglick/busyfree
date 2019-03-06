f0d19d1f5874ecab92f84810a6a7d0a2dd4cda4e
Dan Glick <danjglick@Dans-MacBook-Pro.local>
2019-03-06 01:40:18 -0500

    moved business logic from controllers to models

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,57 +3,18 @@ class Api::V1::UsersController < ApplicationController
 
   def show
     user = User.find(params[:id])
-    Api::V1::UsersController.get_connections(user)
+    user.get_connections
     render json: user
   end
 
   def update
     user = User.find(params[:id])
     if params.key?(:selectedFriend)
-      Api::V1::UsersController.update_friends(user, params)
+      user.update_friends(params)
     elsif params.key?(:busyOrFree)
-      Api::V1::UsersController.update_busy(user, params)
+      user.update_busy(params)
     end
     user.save
     render json: user
-  end
-
-  def self.get_connections(user)
-    connected = false
-    for i in User.all
-      should_connect = (user.busy_or_free == "free" && i.busy_or_free == "free" && user.friends.include?(i.name) && i.friends.include?(user.name))
-      currently_connected = (i.connected_to == user.name)
-      recently_connected = (i.name == user.just_connected || i.just_connected == user.name)
-      if (should_connect || currently_connected) && !recently_connected
-        connected = true
-        user.update({connected_to: i.name, busy_or_free: 'busy'})
-      end
-    end
-    if connected == false
-      user.connected_to = ''
-    end
-  end
-
-  def self.update_friends(user, params)
-    if user.friends.any? {|i| i == params[:selectedFriend]}
-      user.friends.delete(params[:selectedFriend])
-      user.connected_to = ''
-    else
-      for i in User.all
-        if i.name == params[:selectedFriend] && i.name != user.name
-          newFriendsList = user.friends << params[:selectedFriend]
-          user.update(friends: newFriendsList)
-        end
-      end
-    end
-  end
-
-  def self.update_busy(user, params)
-    if params[:busyOrFree] == "free"
-      user.update({busy_or_free: "busy", connected_to: ''})
-    elsif params[:busyOrFree] == "busy"
-      user.just_connected = user.connected_to
-      user.update({busy_or_free: "free", connected_to: ''})
-    end
   end
 end
--- app/models/User.rb
+++ app/models/User.rb
@@ -2,4 +2,43 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :phone, presence: true, uniqueness: true
   validates :password, presence: true, uniqueness: true
+
+  def get_connections
+    connected = false
+    for i in User.all
+      should_connect = (self.busy_or_free == "free" && i.busy_or_free == "free" && self.friends.include?(i.name) && i.friends.include?(self.name))
+      currently_connected = (i.connected_to == self.name)
+      recently_connected = (i.name == self.just_connected || i.just_connected == self.name)
+      if (should_connect || currently_connected) && !recently_connected
+        connected = true
+        self.update({connected_to: i.name, busy_or_free: 'busy'})
+      end
+    end
+    if connected == false
+      self.connected_to = ''
+    end
+  end
+
+  def update_friends(params)
+    if self.friends.any? {|i| i == params[:selectedFriend]}
+      self.friends.delete(params[:selectedFriend])
+      self.connected_to = ''
+    else
+      for i in User.all
+        if i.name == params[:selectedFriend] && i.name != self.name
+          newFriendsList = self.friends << params[:selectedFriend]
+          self.update(friends: newFriendsList)
+        end
+      end
+    end
+  end
+
+  def update_busy(params)
+    if params[:busyOrFree] == "free"
+      self.update({busy_or_free: "busy", connected_to: ''})
+    elsif params[:busyOrFree] == "busy"
+      self.just_connected = self.connected_to
+      self.update({busy_or_free: "free", connected_to: ''})
+    end
+  end
 end
