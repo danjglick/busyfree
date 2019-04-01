9936206897c2f51813eb6fa86f0b19fa7ab3a7c6
Dan Glick <danjglick@Dans-MacBook-Pro.local>
2019-04-01 13:07:35 -0400

    conditional account settings;

--- react/src/components/About.js
+++ react/src/components/About.js
@@ -9,18 +9,18 @@ const About = props => {
       <p>
         <span id="notification">busyfree </span>
         is a simpler way
-        to connect with others.
+        to connect with others
       </p>
       <p>
         just add your friends, then tap the big
-        yellow button whenever you&apos;re free.
+        yellow button whenever you&apos;re free
       </p>
       <p>
         we&apos;ll notify you whenever you and
-        your friends are free at the same time.
+        your friends are free at the same time
       </p>
       <p>
-        &copy;2019. Dan Glick (danjglick@gmail.com)
+        &copy;2019 Dan Glick (danjglick@gmail.com)
       </p>
     </div>
   )
--- react/src/components/AccountSettings.js
+++ react/src/components/AccountSettings.js
@@ -3,30 +3,35 @@ import React, {Component} from 'react'
 class AccountSettings extends Component {
   constructor(props) {
     super(props)
-    this.state = {WelcomeMsg: ''}
+    this.state = {signedIn: false}
   }
 
   componentDidMount() {
     let urlSplit = window.location.href.split('/')
     let userId = urlSplit[urlSplit.length - 2]
-    if (userId != 1) {
-      fetch(`/api/v1/users/${userId}`)
-      .then(response => response.json())
-      .then(body => {
-        this.setState({welcomeMsg: `welcome, ${body.name}`})
-      })
-    } else {
-
-    }
+    fetch(`/api/v1/users/${userId}`)
+    .then(response => response.json())
+    .then(body => {
+      if (body.id != 1) {
+        this.setState({signedIn: true})
+      }
+    })
   }
 
   render() {
     if (this.props.clickedHeading != "Account") {
       return null
     }
+    let signInMsg = 'youre signed-in'
+    if (!this.state.signedIn) {
+      signInMsg = <span>
+        <p> sign-up </p>
+        <p> sign-in </p>
+      </span>
+    }
     return(
       <div className="settingsContent">
-        <p> {this.state.welcomeMsg} </p>
+        <p> {signInMsg} </p>
         <a className="signout" href='/'>
           sign-out
         </a>
